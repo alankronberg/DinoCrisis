ff8f44385f5402a64bd11bd098d7035ab803a12b
JacobEllisASTATE <jacob.ellis@smail.astate.edu>
2021-02-04 15:52:23 -0600

    Test2(Changed default gamemode to BP-MCube and defaut pawn to BP_VRPlayerPawn)

--- DinoCrisis/Source/DinoCrisis/VRPlayerPawn.cpp
+++ DinoCrisis/Source/DinoCrisis/VRPlayerPawn.cpp
@@ -10,16 +10,14 @@ AVRPlayerPawn::AVRPlayerPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	if (GEngine)
-		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "Player Pawn Base Begin Play", true);
-
 }
 
 // Called when the game starts or when spawned
 void AVRPlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "Player Pawn Base Begin Play", true);
 }
 
 // Called every frame
