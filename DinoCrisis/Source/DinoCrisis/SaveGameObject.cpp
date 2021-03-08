370d9645e09a6fbde924f4fd95f622983c674cae
JacobEllisASTATE <jacob.ellis@smail.astate.edu>
2021-03-08 17:07:28 -0600

    Base for Saving in C++
    Works with player location. Terrain is a work in progress

---
+++ DinoCrisis/Source/DinoCrisis/SaveGameObject.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SaveGameObject.h"
+
+
+USaveGameObject::USaveGameObject() {
+	PlayerLocation = FVector(0.f, 0.f, 0.f);
+}
